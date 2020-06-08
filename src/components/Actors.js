4c902de21e0eb51240a0e0292c304291898a93f0
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-06-08 14:21:17 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,23 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => {
+        return (
+          <div>
+            Actor Name: {actor.name}
+            <br />
+            Movies:
+            <ul>
+              {actor.movies.map(movie => {
+                return (
+                  <li>{movie}</li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      })}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+          return (
+            <div>
+              Movie Title: {movie.title} | Time: {movie.time}
+              <br />
+              Genres:
+              <ul>
+                {movie.genres.map(genre => {
+                  return(
+                  <li>{genre}</li>
+                  )
+                })}
+              </ul>
+            </div>
+          )
+        })}
     </div>
   );
 };
